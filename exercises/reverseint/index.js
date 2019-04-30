a3083f7dbc1f51655c68573f1dd41a3540575d58
rykyou <rkim1214@gmail.com>
2019-04-30 11:00:55 -0400

    Finished reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString().split('').reverse().join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+
+// function reverseInt(n) {
+//   let arr = n.toString().split('')
+//   if (Math.sign(n) === -1) {
+//     arr.shift()
+//     arr.reverse().unshift('-')
+//     return parseInt(arr.join(''))
+//   } else {
+//     return parseInt(arr.reverse().join(''))
+//   }
+// }
