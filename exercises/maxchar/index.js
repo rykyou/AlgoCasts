e17b3359c15f6790d53864a93ad8a9fc8cdd70d7
rykyou <rkim1214@gmail.com>
2019-04-30 11:52:17 -0400

    Finished maxChars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+    if (charMap[char] > max) {
+      maxChar = char;
+      max = charMap[char];
+    }
+  }
+  
+  return maxChar;
+}
+
+maxChar('hello');
 
 module.exports = maxChar;
